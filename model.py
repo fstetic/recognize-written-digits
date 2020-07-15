8693671458ef6e714238c7af522a7a18c85b5182
fstetic <filip.stetic74@gmail.com>
2020-07-15 17:31:11 +0200

    Got coordinates in a matrix with desired dimensions for model

--- model.py
+++ model.py
@@ -4,3 +4,14 @@ import numpy as np
 
 def predict(matrix_coords, height, width):
 	model = tf.keras.models.load_model('first_model')
+	full_coords = list()
+	for i in range(width):
+		full_coords.append([])
+		for j in range(height):
+			full_coords[i].append(matrix_coords.get((j,i),0))
+	kernel = np.ones((20, 20), np.uint8)
+	dilatation = cv2.dilate(np.float32(full_coords), kernel, iterations=2)
+	prediction_image = cv2.resize(dilatation, (28,28), cv2.INTER_NEAREST)
+
+	prediction = model.predict([prediction_image])
+	print(np.argmax(prediction))
