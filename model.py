69030475ea87aa352d5aba61c227901eefeb6fdc
fstetic <filip.stetic74@gmail.com>
2020-07-16 22:01:27 +0200

    Fixed prediction

--- model.py
+++ model.py
@@ -11,7 +11,6 @@ def predict(matrix_coords, height, width):
 			full_coords[i].append(matrix_coords.get((j,i),0))
 	kernel = np.ones((20, 20), np.uint8)
 	dilatation = cv2.dilate(np.float32(full_coords), kernel, iterations=2)
-	prediction_image = cv2.resize(dilatation, (28,28), cv2.INTER_NEAREST)
-
-	prediction = model.predict([prediction_image])
+	prediction_image = cv2.resize(dilatation, (28,28))
+	prediction = model.predict(np.expand_dims(prediction_image, axis=0))
 	print(np.argmax(prediction))
