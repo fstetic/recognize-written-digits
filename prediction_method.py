65d0759f7bf14513cc6f409915da91aaa7477819
fstetic <filip.stetic74@gmail.com>
2020-07-17 15:54:01 +0200

    Changed name of file to avoid naming problems and made it return predictions

--- prediction_method.py
+++ prediction_method.py
@@ -1,9 +1,9 @@
-import tensorflow as tf
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def predict(matrix_coords, height, width):
-	model = tf.keras.models.load_model('first_model')
+def predict(model, matrix_coords, height, width):
+	np.set_printoptions(suppress=True)  # don't use scientific notation
 	full_coords = list()
 	for i in range(width):
 		full_coords.append([])
@@ -13,4 +13,9 @@ def predict(matrix_coords, height, width):
 	dilatation = cv2.dilate(np.float32(full_coords), kernel, iterations=2)
 	prediction_image = cv2.resize(dilatation, (28,28))
 	prediction = model.predict(np.expand_dims(prediction_image, axis=0))
-	print(np.argmax(prediction))
+	plt.imshow(prediction_image)
+	plt.show()
+	return_predictions = dict()
+	for i in range(10):
+		return_predictions[i] = prediction[0][i]*100
+	return {k: v for k, v in sorted(return_predictions.items(), key=lambda item: item[1], reverse=True)}
