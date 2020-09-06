87bffaba945a30d4a0c87c21b59b067af3955386
fstetic <filip.stetic74@gmail.com>
2020-09-06 23:13:41 +0200

    Migrated main to Flask

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import tensorflow as tf
-import tkinter as tk
-from gui import Window
+import os
+from flask import Flask, render_template, request, jsonify
 
-def main():
-	# load in a pretrained model
-	model = tf.keras.models.load_model('model')
+def create_app():
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path, 'prediction.sqlite'),
+	)
 
-	root = tk.Tk()  # initialize a window
-	root.geometry("900x600+{}+{}".format(int(root.winfo_screenwidth()/2-450), int(root.winfo_screenheight()/2-300)))
+	@app.route('/predict')
+	def start():
+		return render_template("template.html")
 
-	# makes elements behave normally when window is resized
-	root.grid_rowconfigure(0,weight=1)
-	root.grid_columnconfigure(0,weight=1)
+	@app.route('/script', methods=['POST'])
+	def predict():
+		data = request.get_json()
+		prediction = {1:12.3, 3:12.444, 2:13.1231, 7:14.124, 9:15.541, 8:11.245, 0:11.321, 6:14.444, 4:4.121, 5:2.21321}
+		return jsonify(prediction)
 
-	icon = tk.PhotoImage(file="icon.png")
-	root.iconphoto(True, icon)
-
-	window = Window(model, root)
-	window.mainloop()
-
-if __name__ == '__main__':
-	main()
+	return app
