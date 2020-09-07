4201f01e59ca1de15d0c3d1c1221ff11837a816e
fstetic <filip.stetic74@gmail.com>
2020-09-07 19:04:03 +0200

    Removed gui.py and finished main

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import os
 from flask import Flask, render_template, request, jsonify
+import prediction_method
+import tensorflow as tf
+global model    # global is needed so model can be loaded only once
 
 def create_app():
 	app = Flask(__name__, instance_relative_config=True)
-	app.config.from_mapping(
-		SECRET_KEY='dev',
-		DATABASE=os.path.join(app.instance_path, 'prediction.sqlite'),
-	)
+	global model
+	model = tf.keras.models.load_model('model')
 
 	@app.route('/predict')
 	def start():
@@ -15,7 +15,7 @@ def create_app():
 	@app.route('/script', methods=['POST'])
 	def predict():
 		data = request.get_json()
-		prediction = {1:12.3, 3:12.444, 2:13.1231, 7:14.124, 9:15.541, 8:11.245, 0:11.321, 6:14.444, 4:4.121, 5:2.21321}
+		prediction = prediction_method.predict(model, data, 500, 600)
 		return jsonify(prediction)
 
 	return app
