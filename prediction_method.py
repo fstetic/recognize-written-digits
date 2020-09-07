3ed99bc7917bbbf190a8682eac3b3a86ab27afe3
fstetic <filip.stetic74@gmail.com>
2020-09-07 19:05:02 +0200

    Added making dict from list and returning sorted predictions in predict function

--- prediction_method.py
+++ prediction_method.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
 
-def predict(model, points_coords, height, width):
+def predict(model, list_of_points, height, width):
 	np.set_printoptions(suppress=True)  # don't use scientific notation
+	points_coords = {(point['row'], point['column']):1 for point in list_of_points}     # get dict from list of points
 	full_image_array = [[points_coords.get((j,i),0) for j in range(height)] for i in range(width)]      # get full matrix from a sparse one
 
 	kernel = np.ones((20, 20), np.uint8)
@@ -10,5 +11,4 @@ def predict(model, points_coords, height, width):
 	image_to_predict = cv2.resize(dilation, (28,28))                              # resize to dimensions used by model
 
 	prediction = model.predict(np.expand_dims(image_to_predict, axis=(0,3)))    # expand to (1,28,28,1) array
-	return_predictions = {i: prediction[0][i]*100 for i in range(10)}       # get percentages of predictions for each class
-	return {k: v for k, v in sorted(return_predictions.items(), key=lambda item: item[1], reverse=True)}
+	return {i: prediction[0][i]*100 for i in range(10)} # return percentage for each class
